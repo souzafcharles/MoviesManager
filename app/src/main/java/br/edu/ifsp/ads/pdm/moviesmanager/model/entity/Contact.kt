2361c9d26e92ecc0fa4ee5ee54b86d3816f5b95e
souzafcharles <souzach@outlook.com>
2022-12-05 19:31:58 -0300

    Refactoring entity contact to movie

--- app/src/main/java/br/edu/ifsp/ads/pdm/moviesmanager/model/entity/Contact.kt
+++ app/src/main/java/br/edu/ifsp/ads/pdm/moviesmanager/model/entity/Contact.kt
@@ -1,22 +1,22 @@
 package br.edu.ifsp.ads.pdm.moviesmanager.model.entity
 
 import android.os.Parcelable
-import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity
-data class Contact(
-    @PrimaryKey(autoGenerate = true)
-    var id: Int?,
-    @NonNull
-    var name: String,
-    @NonNull
-    var address: String,
-    @NonNull
-    var phone: String,
-    @NonNull
-    var email: String,
-): Parcelable
+@Entity(tableName = "movie")
+data class Movie(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "movie_name")
+    var movieName: String,
+    @ColumnInfo(name = "release_year")
+    var movieReleaseYear: Int,
+    var movieStudioOrProducer: String,
+    var movieDuration: Int,
+    var movieWatched: Int,
+    var movieRate: Double?,
+    var movieGenreChoiceId: Int?
+) : Parcelable
