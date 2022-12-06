9826e4cb382f1b6913842f1806d4e1768c986288
souzafcharles <souzach@outlook.com>
2022-12-06 01:20:05 -0300

    Update layout

--- app/src/main/java/br/edu/ifsp/ads/pdm/moviesmanager/adapter/MovieAdapter.kt
+++ app/src/main/java/br/edu/ifsp/ads/pdm/moviesmanager/adapter/MovieAdapter.kt
@@ -1,12 +1,14 @@
 package br.edu.ifsp.ads.pdm.moviesmanager.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import br.edu.ifsp.ads.pdm.moviesmanager.R
 import br.edu.ifsp.ads.pdm.moviesmanager.model.entity.Movie
+
 
 class MovieAdapter(context : Context, private val movieList: MutableList<Movie>) : ArrayAdapter<Movie>(context, R.layout.tile_movie, movieList) {
 
@@ -15,6 +17,23 @@ class MovieAdapter(context : Context, private val movieList: MutableList<Movie>)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val movie = movieList[position]
         var movieTileView = convertView
+        if (movieTileView == null) {
+
+            movieTileView = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.tile_movie, parent, false)
+
+            val tileMovieHolder = TileMovieHolder(
+                movieTileView.findViewById(R.id.nameTv),
+                movieTileView.findViewById(R.id.genreAndYearTv),
+                movieTileView.findViewById(R.id.rateTv),
+            )
+            movieTileView.tag = tileMovieHolder
+        }
+
+        with(movieTileView?.tag as TileMovieHolder) {
+            nameTv.text = movie.movieName
+            genreAndYearTv.text = movie.movieGenre + " - " + movie.movieYear.toString()
+            gradeTv.text = if(movie.movieRate != 11) " Nota: " + movie.movieRate.toString() else " Nota: Não atribuida"
+        }
 
         return movieTileView
     }
